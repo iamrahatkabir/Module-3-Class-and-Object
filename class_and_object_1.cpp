27ffcb31cf649647cb0fe9e3ac6f8b415335db9a
iamrahatkabir <iftyrahatkabir@gmail.com>
2025-05-27 23:44:27 +0600

    Class and Object

--- class_and_object_1.cpp
+++ class_and_object_1.cpp
@@ -28,9 +28,9 @@ int main()
     // cin >> b.name >> b.roll >> b.gpa;
 
     // Taking input with Character Space
-    cin.getline(a.name, 100);
+    cin.getline(a.name, 100); //Taking Character input with Space;
     cin >> a.roll >> a.gpa;
-    cin.ignore();
+    cin.ignore(); // Ignoring Space or Enter input;
 
     cin.getline(b.name, 100);
     cin >> b.roll >> b.gpa;
