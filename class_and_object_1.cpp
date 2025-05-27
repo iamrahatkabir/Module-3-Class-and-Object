8f969ff355b4c56ec9dee801af549814b417c653
iamrahatkabir <iftyrahatkabir@gmail.com>
2025-05-27 23:28:07 +0600

    Class & Object

---
+++ class_and_object_1.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student
+{
+public:
+    char name[100]; // 100 bytes
+    int roll;       // 4 bytes
+    double gpa;     // 8 bytes
+};
+
+int main()
+{
+    //   Student a;
+    //   a.roll = 128;
+    //   a.gpa = 4.5;
+
+    //   char temp[100] = "Sakib";
+    //   strcpy(a.name, temp);
+
+    //   cout<<a.name<<" "<<a.roll<<" "<<a.gpa;
+
+    // Taking Input
+    Student a, b;
+
+    // Normal Inputs
+    // cin >> a.name >> a.roll >> a.gpa;
+    // cin >> b.name >> b.roll >> b.gpa;
+
+    // Taking input with Character Space
+    cin.getline(a.name, 100);
+    cin >> a.roll >> a.gpa;
+    cin.ignore();
+
+    cin.getline(b.name, 100);
+    cin >> b.roll >> b.gpa;
+
+    cout << a.name << " " << a.roll << " " << a.gpa << endl;
+    cout << b.name << " " << b.roll << " " << b.gpa << endl;
+
+    return 0;
+}
