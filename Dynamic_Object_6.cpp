336b79b1594e29a0b05e6881432a10ec79c46136
iamrahatkabir <iftyrahatkabir@gmail.com>
2025-05-28 03:12:25 +0600

    Dynamic Object

---
+++ Dynamic_Object_6.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Student{
+    public:
+    int roll;
+    int cls;
+    double gpa;
+
+    //This is Constructor. it does not need any return type as function;
+    Student(int roll, int cls, double gpa){
+        //Setting the vaule of class;
+        //(this) - it is a pointer
+        // (*this).roll = roll;
+        this->roll = roll;
+        this->cls = cls;
+        this->gpa = gpa;
+    }
+
+};
+
+
+int main()
+{
+    // Passing value to Student() Constructor;
+    // Student rahim(1, 7, 4.17);
+    Student *karim = new Student (4, 6, 4.67);
+
+    // cout << rahim.roll << " " << rahim.cls << " " << rahim.gpa << endl;
+
+    //Dereferencing (*karim).roll = karim->roll;
+    cout << karim->roll << " " << karim->cls << " " << karim->gpa << endl;
+    
+    return 0;
+}
