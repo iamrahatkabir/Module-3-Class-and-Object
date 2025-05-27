1f0d5c49eeeca2e1df26986bdff111d5f9ff4810
iamrahatkabir <iftyrahatkabir@gmail.com>
2025-05-28 00:21:26 +0600

    Class & Object

--- class_and_object_1.cpp
+++ class_and_object_1.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 class Student
 {
-public:
+public: //Privet,Protected;
     char name[100]; // 100 bytes
     int roll;       // 4 bytes
     double gpa;     // 8 bytes
@@ -11,7 +11,7 @@ public:
 
 int main()
 {
-    //   Student a;
+    //   Student a, b;
     //   a.roll = 128;
     //   a.gpa = 4.5;
 
@@ -20,12 +20,13 @@ int main()
 
     //   cout<<a.name<<" "<<a.roll<<" "<<a.gpa;
 
-    // Taking Input
-    Student a, b;
-
     // Normal Inputs
     // cin >> a.name >> a.roll >> a.gpa;
     // cin >> b.name >> b.roll >> b.gpa;
+
+
+    // This is Object
+    Student a, b; 
 
     // Taking input with Character Space
     cin.getline(a.name, 100); //Taking Character input with Space;
